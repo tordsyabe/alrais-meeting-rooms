5015d9edec4476f6352a445e71b362561ed7f796
John Llave <extordsllave@gmail.com>
2021-05-26 17:42:01 +0400

    remove start and end time formatted

--- src/services/SendEmailVerificationService.js
+++ src/services/SendEmailVerificationService.js
@@ -1,10 +1,6 @@
 import { secondsToLocalTime, secondsToLongDate } from "../utils/dateFormatter";
 
 export function sendEmailVerification(data) {
-  data.startTime = secondsToLocalTime(data.startTime.seconds);
-  data.endTime = secondsToLocalTime(data.endTime.seconds);
-  data.meetingDate = secondsToLongDate(data.meetingDate.seconds);
-  console.log(data);
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
