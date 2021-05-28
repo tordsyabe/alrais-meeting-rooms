1b0d81146230f29d57faf6b0b24a5fff6463c372
tordsyabe <46318298+tordsyabe@users.noreply.github.com>
2021-05-28 12:58:51 +0400

    added approved email service

--- src/services/SendEmailVerificationService.js
+++ src/services/SendEmailVerificationService.js
@@ -2,13 +2,27 @@ import { secondsToLocalTime, secondsToLongDate } from "../utils/dateFormatter";
 
 export function sendEmailVerification(data) {
   data.date = secondsToLongDate(data.date.seconds);
- 
+
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ data }),
   };
   fetch("http://192.168.10.15/send-verification-email", requestOptions)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+
+export function sendApprovedEmail(data) {
+  data.date = secondsToLongDate(data.date.seconds);
+
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ data }),
+  };
+  fetch("http://192.168.10.15/send-approved-email", requestOptions)
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((err) => console.log(err));
